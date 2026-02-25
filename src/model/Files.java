17d3fd9e65ab0e3402c133df3f28110b6d993aff
Isabel Victoria Sousa Bria <ivsousa.dev@gmail.com>
2026-02-25 12:22:07 +0100

    create files

---
+++ src/model/Files.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author admin
+ */
+public class Files {
+     public static void EscrituraFichero(ArrayList<Product> inventory) {
+         
+     }
+     public static void LecturaFichero(ArrayList<Product> inventory) {
+         
+     }
+    
+}
