a38e4b6b9c19357c28aafe04c3f0c926eea4e78e
Isabel Victoria Sousa Bria <ivsousa.dev@gmail.com>
2026-03-19 09:09:20 +0100

    writeSales and filreader done

--- src/model/Files.java
+++ src/model/Files.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,20 +20,30 @@ import java.util.List;
  */
 public class Files {
 
-    public static void FileWriter(ArrayList<Product> inventory) throws IOException {
+    public static void writeSales(ArrayList<Sale> sales)  {
 
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String dateFormatted = date.format(formatter);
+        
+        String fileName = "sales_" + dateFormatted + ".txt";
+                
         FileWriter outputStreamInventory;
         BufferedWriter out1 = null;
 
+        int index = 1;
+
         try {
-            outputStreamInventory = new FileWriter("inputInventory.txt");
+            outputStreamInventory = new FileWriter(fileName);
             out1 = new BufferedWriter(outputStreamInventory);
 
-            for (Product product : inventory) {
+            for (Sale sale : sales) {
 
-                out1.write("PRODUCT:" + "\t" + product.getName() + "\t" + "WHOLESALER PRICE:" + "\t" + product.getPublicPrice() + "\t" + "STOCK:" + "\t" + product.getStock() + "\t");
+                out1.write(index + ";" + "Client=" + sale.getClient().getName() + ";" + "\n" + index + ";" + "Products=" + sale.getProducts() + ";" + "\n" + index + ";" + "Amount=" + sale.getAmount().getValue() + " euro;" + "\t");
 
                 out1.write("\n");
+
+                index++;
             }
 
         } catch (java.io.FileNotFoundException ex) {
@@ -54,9 +65,9 @@ public class Files {
 
         String line;
 
-        String name;
-        Amount wholesalerPrice;
-        int stock;
+        String name = "";
+        Amount wholesalerPrice = new Amount(0);
+        int stock = 0;
 
         FileReader inputStream = null;
         BufferedReader in = null;
@@ -64,21 +75,28 @@ public class Files {
         try {
             inputStream = new FileReader("inputInventory.txt");
             in = new BufferedReader(inputStream);
-
+            
             while ((line = in.readLine()) != null) {
-
+               
                 String data[] = line.split(";");
                 //crear un arraylist para poner un objeto
 
-                for (int i = 0; i < data.length - 1; i++) {
-                    String finalData[] = line.split(":");
-
-                    name = finalData[1];
-                    wholesalerPrice = new Amount(Double.parseDouble(finalData[3]));
-                    stock = Integer.parseInt(finalData[5]);
-                    
-                inventory.add(new Product(name, wholesalerPrice, stock));
+                for (int i = 0; i < data.length; i++) {
+                    String finalData[] = data[i].split(":");
+                    switch (i) {
+                        case 0:
+                            name = finalData[1];
+                            break;
+                        case 1:
+                            wholesalerPrice = new Amount(Double.parseDouble(finalData[1]));
+                            break;
+                        case 2:
+                            stock = Integer.parseInt(finalData[1]);
+                            break;
+                        
+                    }
                 }
+                inventory.add(new Product(name, wholesalerPrice, true, stock));
             }
 
         } catch (java.io.IOException ex) {
